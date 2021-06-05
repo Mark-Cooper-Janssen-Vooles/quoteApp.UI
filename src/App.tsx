9e69de95b1f83aacbacdad2f9f650630efa9a0a6
quote-app.ui <m.janssen-vooles@outlook.com.au>
2021-06-05 10:43:41 +1000

    Added buildspec.yml

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,29 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-      <div>
 
+      <div>
+        <button>Add new Quote</button>
       </div>
       <Header />
+
+      <div>
+        <h4>draft quotes</h4>
+        <div>
+          <p>existing quote 1</p>
+          <button>edit quote</button>
+          <button>delete quote</button>
+          <button>finalise and send</button>
+        </div>
+      </div>
+
+      <div>
+        <h4>sent quotes</h4>
+        <div>
+          <p>existing quote 1</p>
+        </div>
+      </div>
+
     </div>
   )
 }
