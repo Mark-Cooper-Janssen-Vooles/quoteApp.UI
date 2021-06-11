eb4c10b8bfed5cf59a3ca4eca0ab5b860371dd1d
quote-app.ui <m.janssen-vooles@outlook.com.au>
2021-06-11 15:21:51 +1000

    Added quote form close functionality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import QuoteForm from "./components/QuoteForm";
 
 test('renders learn react link', () => {
   render(<App />);
@@ -8,4 +9,23 @@ test('renders learn react link', () => {
 
   //worlds worst unit test
   expect(title.innerHTML).toContain("Quote App");
+});
+
+test('clicking cancel button removes component', () => {
+  //need to add store to get this to work!!
+
+  // render(<App />);
+  // console.log(screen.debug())
+  // const addNewQuote = screen.getByText(/Add new Quote/i);
+  // addNewQuote.click();
+  //
+  // const newQuoteForm = screen.getByText('Add a new quote:')
+  //
+  // expect(newQuoteForm).toBeTruthy();
+
+
+  //
+  // const cancel = screen.getByText('Cancel new quote');
+  // cancel.click();
+  // expect(ca).toBeTruthy();
 });
--- src/components/QuoteForm.tsx
+++ src/components/QuoteForm.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import './QuoteCard.css'
+import {setQuoteFormActive} from "../redux/actions/quoteActions";
+import {connect} from "react-redux";
 
-const QuoteForm = () => {
+interface IQuoteForm {
+    setQuoteFormActive: (bool: boolean) => void
+}
+const QuoteForm: React.FC<IQuoteForm> = (props) => {
+    const removeQuoteForm = () => {
+        props.setQuoteFormActive(false);
+    }
     return (
         <div className="quote-card">
             <h4>Add a new quote:</h4>
@@ -9,8 +17,14 @@ const QuoteForm = () => {
             <p style={{textAlign: "left"}}>amount: </p>
             <p style={{textAlign: "left"}}>contact: </p>
             <button>Save draft quote</button>
-            <button>Cancel new quote</button>
+            <button onClick={removeQuoteForm}>Cancel new quote</button>
         </div>);
 };
 
-export default QuoteForm;
+const mapStateToProps = (state: any) => {}
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        setQuoteFormActive: (show: boolean) => dispatch(setQuoteFormActive(show))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(QuoteForm)
